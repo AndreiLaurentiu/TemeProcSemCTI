de287093daa12353c29c4447322c8f878f72682a
AndreiLaurentiu <81575871+AndreiLaurentiu@users.noreply.github.com>
2023-12-10 19:26:44 +0200

    lab 8 final

--- lab8/ex1.py
+++ lab8/ex1.py
@@ -1,47 +1,52 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from statsmodels.tsa.ar_model import AutoReg
 
 
 def ex1():
-    # a
+    #a
     n = 1000
-    x = np.arange(n)
+    x = np.arange(n, dtype=np.float64)
 
     trend = -3 * x**2 + 15 * x - 2
     sezon = np.cos(2 * np.pi * 4 * x) + np.sin(2 * np.pi * 2 * x)
     res = np.random.rand(n)
-
-    trend *= 1e-4
-    sezon *= 1e10
-
     serie = trend + sezon + res
 
     _, axs = plt.subplots(4, figsize=(10, 10))
     plt.suptitle('Serie de timp')
-
     axs[0].plot(x, serie, 'y')
     axs[0].set_ylabel('Serie')
-
     axs[1].plot(x, trend, 'b')
     axs[1].set_ylabel('Trend')
-
     axs[2].plot(x, sezon, 'r')
     axs[2].set_ylabel('Sezon')
-
     axs[3].plot(x, res, 'gray')
     axs[3].set_ylabel('Res')
 
-    # b
+    #b
     auto = np.correlate(serie, serie, mode='full')
-    lags = np.arange(-n + 1, n)
-
+    auto = auto[auto.size // 2:]
     plt.figure(2)
-
-    plt.plot(lags, auto, 'y')
-
+    plt.plot(x, auto, 'y')
     plt.title('Autocorelatie')
     plt.grid()
 
-    # d
+    #c
+    p = 10
+    model = AutoReg(serie, lags=p)
+    model_fit = model.fit()
+
+    prediction_length = 1000
+    predictions = model_fit.predict(start=n, end=n + prediction_length - 1, dynamic=False)
+
+    plt.figure(3)
+    plt.title('Seria de timp și predicțiile sale')
+    plt.plot(x, serie, 'black', label='Seria de timp')
+    plt.plot(np.arange(n, n + prediction_length), predictions, 'red', label='Model AR')
+    plt.legend()
 
     plt.show()
+
+
+
--- lab8/main.py
+++ lab8/main.py
@@ -4,6 +4,5 @@ import ex1
 def main():
     ex1.ex1()
 
-
 if __name__ == "__main__":
     main()
